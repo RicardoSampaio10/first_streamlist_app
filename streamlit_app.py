af69b1fa74d3ba6fa585f744315c32ccea9ee213
RicardoSampaio10 <139780627+RicardoSampaio10@users.noreply.github.com>
2023-07-17 11:21:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
-streamlit.text('Breakfast Menu')
+streamlit.title('My Mom's new health Diner')
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
